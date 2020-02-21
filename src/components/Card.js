1b4d65895390205757b1466302b30128656dc3b3
Darren Ash <darren.ash@fujifilm.com>
2020-02-21 09:56:29 +0000

    First commit

---
+++ src/components/Card.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Card = ({name,email,id}) => {
+
+	return (
+				<div className='tc bg-light-blue dib  pa3 ma3 grow bw2 shadow-5'>
+				<img alt='robots' src={`https://robohash.org/${id}`}/>
+				<h2>{name}</h2>
+				<p>{email}</p>
+			</div>
+		);
+}
+
+export default Card;
---
+++ src/robots.js
@@ -0,0 +1,51 @@
+export const robots = [
+  {
+    id: 1,
+    name: 'Darren Ash',
+    username: 'Darren',
+    email: 'Darren.Ash@Fujifilm.com'
+  },
+  {
+    id: 2,
+    name: 'Julian Trusty',
+    username: 'Julian',
+    email: 'Julian.Trusty@Fujifilm.com'
+  },
+  {
+    id: 3,
+    name: 'Paul Scott',
+    username: 'Paul',
+    email: 'Paul.Scott@Fujifilm.com'
+  },
+  {
+    id: 4,
+    name: 'Graham Beerling',
+    username: 'Graham',
+    email: 'Graham.Beerling@Fujifilm.com'
+  },
+  {
+    id: 5,
+    name: 'Andy Gutteridge',
+    username: 'Andy',
+    email: 'Andrew.Gutteridge@Fujifilm.com'
+  },
+  {
+    id: 6,
+    name: 'Lydia Blissenden',
+    username: 'Lydia',
+    email: 'Lydia.Blissenden@Fujifilm.com'
+  },
+  {
+    id: 7,
+    name: 'Fred Bloggs',
+    username: 'Fred',
+    email: 'Fred.Bloggs@Fujifilm.com',
+  },
+  {
+    id: 8,
+    name: 'Santa Claus',
+    username: 'Santa',
+    email: 'Santa.Claus@Fujifilm.com'
+  }
+
+];
